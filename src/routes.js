cee327b56511a7c59cd03d3cae08e235c3391f3f
murillo.nahas <murillonahas@hotmail.com>
2023-04-11 16:55:44 -0300

    feat: crud operations

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,12 @@ const router = Router();
 
 router.get("/contacts", ContactController.index);
 
+router.get("/contacts/:id", ContactController.show);
+
+router.post("/contacts", ContactController.store);
+
+router.delete("/contacts/:id", ContactController.delete);
+
+router.put("/contacts/:id", ContactController.update);
+
 module.exports = router;
