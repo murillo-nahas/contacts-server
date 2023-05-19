918970ec9dfbcfe5657148dc177c0232f2861ec4
murillo.nahas <murillonahas@hotmail.com>
2023-05-19 13:36:47 -0300

    feat: cors middleware index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
+const cors = require("./app/middlewares/cors");
+const errorHandler = require("./app/middlewares/errorHandler");
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
+app.use(errorHandler);
 
-app.listen(3000, () => console.log("Server started at http://localhost:3000"));
+app.listen(3001, () => console.log("Server started at http://localhost:3001"));
