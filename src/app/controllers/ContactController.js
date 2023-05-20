3b09af69568175a7fc81488866013eb610998eb7
murillo.nahas <murillonahas@hotmail.com>
2023-05-20 18:48:09 -0300

    feat: sending statusCode in response

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -14,7 +14,7 @@ class ContactController {
     const contact = await ContactsRepository.findById(id);
 
     if (!contact) {
-      return response.status(404).json({ error: "User not found" });
+      return response.status(404).json({ error: "Contact not found" });
     }
 
     response.json(contact);
@@ -42,7 +42,7 @@ class ContactController {
       category_id,
     });
 
-    response.json(contact);
+    response.status(201).json(contact);
   }
 
   async update(request, response) {
@@ -52,7 +52,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findById(id);
 
     if (!contactExists) {
-      return response.status(400).json({ error: "User not found" });
+      return response.status(400).json({ error: "Contact not found" });
     }
 
     if (!name) {
